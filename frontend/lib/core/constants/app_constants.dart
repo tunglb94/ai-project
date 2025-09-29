87479a35d497b38cde54af29b96c2d063f1e7a53
Lee <tunglb94@gmail.com>
2025-09-29 12:52:42 +0700

    Add requirements.txt to root

--- frontend/lib/core/constants/app_constants.dart
+++ frontend/lib/core/constants/app_constants.dart
@@ -3,7 +3,7 @@
 class AppConstants {
   // QUAN TRỌNG:
   // Nếu bạn chạy app trên máy ảo Android, dùng URL này
-static const String baseUrl = 'http://10.0.2.2:8000/api/v1';
+static const String baseUrl = "https://ai-project.onrender.com/api/v1";
   // Nếu bạn chạy app trên máy ảo iOS, dùng URL này
   // static const String baseUrl = 'http://localhost:8000/api/v1';
 
